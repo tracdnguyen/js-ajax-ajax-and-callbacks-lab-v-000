19398131a2ae1cf222531c7fe694cd2e15387fe9
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-18 03:01:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function displayError(){
 function searchRepositories(){
   let seachTerms = $('#searchTerms').val();
   let url = `https://api.github.com/search/repositories?q=${searchTerms}/`
+  $.get(url, function(data){
+    $("#results").html(showRepositories(data))
+  }).fail(error => {displayError()}
+}
+
+function showRepositories(data){
+  let repo = data.items.map(r => renderRepo(r))
+  return `<ul>${repo}</ul>`
 }
 
 $(document).ready(function (){
