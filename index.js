d3f1c1a496a3f5cbeaa8d31ce1a10cf1149ac16d
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-18 03:19:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
+function displayError(){
+    $('#errors').html("Sorry, there was an error. Please try again.")
+}
+function searchRepositories(){
+    let searchTerms = $('#searchTerms').val();
+    let url = `https://api.github.com/search/repositories?q=${searchTerms}/`
+    $.get(url, function(response){
+        $("#results").html(showRepositories(response))
+    }).fail(error =>{
+        displayError()
+    })
+}
+
+function showRepositories(response){
+    const repos = response.items.map(repo => renderRespositories(repo))
+    return `<ul>${repos}</ul>`
+}
+
+function renderRespositories(repo){
+    return `<li><b>Name: </b><spanp>${repo.name}</span><br>
+    <b>Description: </b><span>${repo.description}</span><br>
+    <b>URL: </b><span>${repo.html_url}</span>
+    <p><a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+    </li><br>
+    `
+}
+
+function showCommits(repo){
+    $.get(`https://api.github.com/repos/${repo.dataset.owner}/${repo.dataset.repository}/commits`, data => {
+      $('#details').html(renderCommits(data))
+    }).fail(error => {
+      displayError()
+    })
+  }
+
+function renderCommits(data){
+    let commits = data.map(commit => addHtml(commit))
+    return `<ul>${commits}</ul>`
+}
+
+function addHtml(commit){
+    return `<li><img src=${commit.author.avatar_url} height="50" width="50"><br>
+    <b>Author: </b><span>${commit.author.login}</span><br>
+    <b>SHA: </b><span>${commit.sha}</span><br>
+    <b>URL: </b><span>${commit.html_url}></span></li><br>`
+}
 $(document).ready(function (){
 });
